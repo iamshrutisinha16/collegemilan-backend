7a31d8219dbd3692b223819072c2ff1ceb26920c
shrutisinha74168 <shrutisinha74168@gmail.com>
2026-03-22 11:54:20 +0530

    chnges in multer

--- config/multer.js
+++ config/multer.js
@@ -1,4 +1,37 @@
-const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+// 1. Cloudinary Configuration (Ye details aapko Cloudinary dashboard se milengi)
+cloudinary.config({
+  cloud_name: 'dfkbi63ro', 
+  api_key: '513638623697344',      
+  api_secret: '**********' 
+});
+
+// 2. Cloudinary Storage Setup
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'milan_education', // Is naam ka folder Cloudinary par ban jayega
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+    public_id: (req, file) => {
+      const safeFileName = file.originalname.replace(/\s+/g, '-').split('.')[0];
+      return Date.now() + "-" + safeFileName;
+    }
+  },
+});
+
+const upload = multer({ 
+  storage: storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024 // 10 MB (Cloudinary free tier ke liye kaafi hai)
+  }
+});
+
+module.exports = upload;
+
+/*const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
@@ -21,4 +54,4 @@ const upload = multer({
   }
 });
 
-module.exports = upload;
+module.exports = upload;*/
